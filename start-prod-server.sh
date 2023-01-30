53c58aa7851b7c96368aca488308a6001fe189a2
myanzik <myanz.470shrestha@gmail.com>
2023-01-30 13:41:01 +0545

    test server start script

--- start-prod-server.sh
+++ start-prod-server.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
   
-while true
-do
-response=$(curl --max-time 15 --write-out %{http_code} --silent --output /dev/null https://tayaba-api.xa.rahat.io/api/v1/settings)
-# ((i++))
-    if [ "$response" = "200" ]; then
-        echo "rahat server is ready";
-        yarn production;
-        break;
-    else
-        echo "rahat server is not ready";
-        sleep 5;
-    fi
-done
+# while true
+# do
+# response=$(curl --max-time 15 --write-out %{http_code} --silent --output /dev/null https://tayaba-api.xa.rahat.io/api/v1/settings)
+# # ((i++))
+#     if [ "$response" = "200" ]; then
+#         echo "rahat server is ready";
+#         yarn production;
+#         break;
+#     else
+#         echo "rahat server is not ready";
+#         sleep 5;
+#     fi
+# done
+
+yarn production
